e2aa64ab135ecedabb7d42a3364ecab46e0dbad0
Kejukalariya <kejukalariya5767@gmail.com>
2025-01-18 05:46:20 +0000

    database connected

--- signUpbackend.php
+++ signUpbackend.php
@@ -6,4 +6,12 @@ $password = ($_POST['password']);
 
 $conn = mysqli_connect('localhost', 'root', '', 'career_guidance_2.0');
 
+$query = "INSERT INTO `signup`(`first_name`, `last_name`, `email_address`, `password`) VALUES ('$first_name','$last_name','$email_address','$password')";
+
+$result = mysqli_query($conn, $query);
+
+if($result){
+    echo '<script>alert("Registered Successfully");</script>';
+    echo '<script>window.location.href="login.php"</script>';
+}
 ?>

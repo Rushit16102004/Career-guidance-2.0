1b287bd47f970cd76ca35f51f981f2ad7bb930f3
Kejukalariya <kejukalariya5767@gmail.com>
2025-01-18 06:01:21 +0000

    login backend created

--- loginbackend.php
+++ loginbackend.php
@@ -1,3 +1,22 @@
 <?php
+$email_address = ($_POST['email']);
+$password = ($_POST['password']);
 
+$conn = mysqli_connect('localhost', 'root', '', 'career_guidance_2.0');
+
+$query = "SELECT * FROM `signup` WHERE `email` = '$email_address' AND `password` = '$password'";
+
+$result = mysqli_query($conn, $query);
+
+$data = mysqli_fetch_row($result);
+
+print_r($data);
+
+if($data){
+    echo '<script>alert("Login Successfully");</script>';
+    echo '<script>window.location.href="interest.php"</script>';
+}else{
+    echo '<script>alert("Account does not exist");</script>';
+    echo '<script>window.location.href="login.php"</script>';
+}
 ?>

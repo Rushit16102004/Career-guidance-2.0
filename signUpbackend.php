3c71d0048d64eb02226db85e4f09b4e7c69c6d90
Kejukalariya <kejukalariya5767@gmail.com>
2025-01-18 05:37:17 +0000

    database

--- signUpbackend.php
+++ signUpbackend.php
@@ -1,3 +1,9 @@
 <?php
+$first_name = ($_POST['firstname']);
+$last_name = ($_POST['lastname']);
+$email_address = ($_POST['email']);
+$password = ($_POST['password']);
+
+$conn = mysqli_connect('localhost', 'root', '', 'career_guidance_2.0');
 
 ?>

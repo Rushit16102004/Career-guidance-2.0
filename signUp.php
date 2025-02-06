84af4d433eb52e3d5b206f8800d82b6dab2c269a
Kejukalariya <kejukalariya5767@gmail.com>
2025-02-06 04:51:26 +0000

    submit button removed

--- signUp.php
+++ signUp.php
@@ -35,7 +35,6 @@
             <label for="exampleInputEmail1">Confirm Password</label>
             <input type="password" name="cpassword" id="Cpassword"  class="form-control" aria-describedby="emailHelp" placeholder="Enter Password">
         </div>
-        <button type="submit">Submit</button>
         <div class="col-md-12 text-center mb-3">
             <a href="/login.php"></a>
         </div>
